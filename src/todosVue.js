56471752ee32b07cf12ea6792516b1f12f5b386d
alex <alexbonavila44@gmail.com>
2017-05-29 18:37:40 +0200

    Fent servir todos backend explotació

--- src/todosVue.js
+++ src/todosVue.js
@@ -1,10 +1,10 @@
 const todosVue = {
   STORAGE_TOKEN_KEY: 'token',
-  OAUTH_CLIENT_ID: 3,
+  OAUTH_CLIENT_ID: 4,
   OAUTH_REDIRECT_URI: 'http://localhost:8095/login',
-  OAUTH_SERVER_URL: 'http://client.dev:8080/oauth/authorize?',
-  API_TASK_URL: 'http://client.dev:8080/api/v1/task',
-  API_PROFILE_URL: 'http://client.dev:8080/api/v1/user'
+  OAUTH_SERVER_URL: 'http://todosbackend.tk/oauth/authorize?',
+  API_TASK_URL: 'http://todosbackend.tk/api/v1/task',
+  API_PROFILE_URL: 'http://todosbackend.tk/api/v1/user'
 
   // ANDROID_SENDER_ID: '',
   // REGISTER_GCM_TOKEN_URL: '',
